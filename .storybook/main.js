92b62a35d506443ff13d741e9706d85f96c50d94
bipinparajuli <bipinprjl@gmail.com>
2021-08-17 19:11:57 +0545

    complete not yet deployed and tested

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,7 +3,7 @@ module.exports = {
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)",
     "../src/**/*.stories.js",
-    '../src/components/**/*.stories.js',
+    '../src/Component/**/*.stories.js',
 
   ],
   "addons": [
